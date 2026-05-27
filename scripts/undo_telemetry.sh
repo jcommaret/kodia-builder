b97c1107701b271bc22bfdbe1b293c067108a365
Jérôme Commaret <jcommaret@gmail.com>
2026-05-27 18:26:13 +0200

    Enhance undo_telemetry.sh by adding exclusion patterns for compiled output directories in search commands. This prevents modifications to pre-compiled JS files, ensuring source map integrity during the NLS build step.

--- scripts/undo_telemetry.sh
+++ scripts/undo_telemetry.sh
@@ -34,16 +34,20 @@ export -f replace_with_debug
 
 d1=$( date +%s )
 
+# Exclude compiled output dirs: modifying pre-compiled JS breaks source map integrity checks in the NLS build step.
+RG_EXCLUDE=(--glob '!out/**' --glob '!out-build/**' --glob '!out-vscode/**' --glob '!out-vscode-min/**' --glob '!.build/**' --glob '!node_modules/**')
+GREP_EXCLUDE=(--exclude-dir=.git --exclude-dir=out --exclude-dir=out-build --exclude-dir=out-vscode --exclude-dir=out-vscode-min --exclude-dir=.build --exclude-dir=node_modules)
+
 if [[ "${OS_NAME}" == "linux" ]]; then
   if [[ ${VSCODE_ARCH} == "x64" ]]; then
-    ./node_modules/@vscode/ripgrep/bin/rg --no-ignore -l "${SEARCH}" . | xargs -I {} bash -c 'replace_with_debug "${1}" "{}"' _ "${REPLACEMENT}"
+    ./node_modules/@vscode/ripgrep/bin/rg --no-ignore "${RG_EXCLUDE[@]}" -l "${SEARCH}" . | xargs -I {} bash -c 'replace_with_debug "${1}" "{}"' _ "${REPLACEMENT}"
   else
-    grep -rl --exclude-dir=.git -E "${SEARCH}" . | xargs -I {} bash -c 'replace_with_debug "${1}" "{}"' _ "${REPLACEMENT}"
+    grep -rl "${GREP_EXCLUDE[@]}" -E "${SEARCH}" . | xargs -I {} bash -c 'replace_with_debug "${1}" "{}"' _ "${REPLACEMENT}"
   fi
 elif [[ "${OS_NAME}" == "osx" ]]; then
-  ./node_modules/@vscode/ripgrep/bin/rg --no-ignore -l "${SEARCH}" . | xargs -I {} bash -c 'replace_with_debug "${1}" "{}"' _ "${REPLACEMENT}"
+  ./node_modules/@vscode/ripgrep/bin/rg --no-ignore "${RG_EXCLUDE[@]}" -l "${SEARCH}" . | xargs -I {} bash -c 'replace_with_debug "${1}" "{}"' _ "${REPLACEMENT}"
 else
-  ./node_modules/@vscode/ripgrep/bin/rg --no-ignore -l "${SEARCH}" . | xargs -I {} bash -c 'replace_with_debug "${1}" "{}"' _ "${REPLACEMENT}"
+  ./node_modules/@vscode/ripgrep/bin/rg --no-ignore "${RG_EXCLUDE[@]}" -l "${SEARCH}" . | xargs -I {} bash -c 'replace_with_debug "${1}" "{}"' _ "${REPLACEMENT}"
 fi
 
 d2=$( date +%s )
